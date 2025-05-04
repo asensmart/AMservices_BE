0ae9dd590202b042bbee99f8a8a3b147a4fe48e4
asensmart <asenali.g@dyooti.com>
2025-05-04 16:48:10 +0530

    blog API Testing

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-require('dotenv').config()
-const ConnectMongoDB = require('./config/mongoose')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const Router = require('./routers/index.js')
-const cookieParser = require('cookie-parser')
+require("dotenv").config();
+const ConnectMongoDB = require("./config/mongoose");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const Router = require("./routers/index.js");
+const cookieParser = require("cookie-parser");
 
-app.use(cors({ credentials: true, origin: true }))
-app.use(cookieParser())
-app.use(express.json())
+app.use(cors({ credentials: true, origin: true }));
+app.use(cookieParser());
+app.use(express.json());
 
-app.use('/uploads/brands', express.static(__dirname + '/uploads/brands/'))
-app.use('/api/v1/', Router)
+app.use("/uploads/brands", express.static(__dirname + "/uploads/brands/"));
+app.use("/api/v1/", Router);
 
-const PORT = process.env.PORT
+app.get("/", (req, res) => {
+  res.send("Welcome to the API");
+});
 
-ConnectMongoDB()
+const PORT = process.env.PORT;
 
-app.listen(PORT, err => {
-  if (err) return console.log('Server Error! 500')
-  console.log(`Server running on port ${PORT}`)
-})
+ConnectMongoDB();
+
+app.listen(PORT, (err) => {
+  if (err) return console.log("Server Error! 500");
+  console.log(`Server running on port ${PORT}`);
+});
